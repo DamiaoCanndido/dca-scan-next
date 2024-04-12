0517013abefd30db11c9139f3c6371fad50ec3bc
DamiaoCanndido <tower_gamer@hotmail.com>
2024-04-12 15:24:07 -0300

    team table

--- src/components/owner/search.tsx
+++ src/components/owner/search.tsx
@@ -1,10 +1,12 @@
-import { AiOutlineSearch } from 'react-icons/ai';
+import { LuSearch } from 'react-icons/lu';
 
 export const Search = () => {
   return (
-    <div className="flex fixed mt-16 ml-48 gap-3 h-12 items-center px-3 w-72 py-1.5 border border-green-700 rounded-lg text-sm max-lg:ml-0">
-      <AiOutlineSearch size={30} color="green" />
-      <input className="flex-1 outline-none" />
+    <div className="flex fixed mt-16 ml-48 gap-3 h-12 items-center px-3 w-auto py-1.5 border border-green-700 rounded-lg text-sm max-lg:ml-0">
+      <button>
+        <LuSearch size={20} color="green" />
+      </button>
+      <input className="flex-1 border-0" />
     </div>
   );
 };
--- src/components/owner/teams.tsx
+++ src/components/owner/teams.tsx
@@ -1,37 +1,88 @@
 import Image from 'next/image';
+import {
+  LuMoreHorizontal,
+  LuChevronLeft,
+  LuChevronsLeft,
+  LuChevronRight,
+  LuChevronsRight,
+} from 'react-icons/lu';
 
 export const Teams = () => {
   return (
-    <table className="mt-28 ml-48 max-lg:ml-0">
-      <thead className="">
+    <table className="border w-full border-green-600 rounded-lg mt-28 ml-48 max-lg:ml-0">
+      <thead className="border-b border-green-600">
         <tr>
-          <th>
-            <input type="checkbox" />
+          <th style={{ width: 64 }} className="text-left pl-2 py-3">
+            <input
+              type="checkbox"
+              className="size-4 rounded border border-green-600"
+            />
           </th>
-          <th>Escudo</th>
-          <th>Nome</th>
-          <th>Código</th>
-          <th></th>
+          <th className="text-left">Escudo</th>
+          <th className="text-left">Nome</th>
+          <th className="text-left">Código</th>
+          <th className="text-left"></th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>
-            <input type="checkbox" />
-          </td>
-          <td>
-            <Image
-              src="https://seeklogo.com/images/B/Brazil-logo-FDA32A35FD-seeklogo.com.png"
-              width={20}
-              height={20}
-              alt=""
-            />
-          </td>
-          <td>Brasil</td>
-          <td>BRA</td>
-          <td></td>
-        </tr>
+        {Array.from({ length: 30 }).map((_, i) => {
+          return (
+            <tr
+              key={i}
+              className="border-b border-green-600 hover:bg-green-100"
+            >
+              <td style={{ width: 64 }} className="pl-2 py-2.5">
+                <input
+                  type="checkbox"
+                  className="size-4 rounded border border-green-600"
+                />
+              </td>
+              <td>
+                <Image
+                  src="https://seeklogo.com/images/B/Brazil-logo-FDA32A35FD-seeklogo.com.png"
+                  width={30}
+                  height={30}
+                  alt=""
+                />
+              </td>
+              <td>Brasil</td>
+              <td>BRA</td>
+              <td style={{ width: 64 }}>
+                <button className="border bg-green-100 border-green-600 rounded-md p-1.5">
+                  <LuMoreHorizontal color="green" size={20} />
+                </button>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
+      <tfoot>
+        <tr>
+          <td className="pl-2 py-2.5" colSpan={2}>
+            Mostrando 10 de 28 itens
+          </td>
+
+          <td className="pr-2 text-right" colSpan={3}>
+            <div className="inline-flex items-center gap-8">
+              <span>Página 1 de 2</span>
+              <div className="flex gap-1">
+                <button className="border bg-green-100 border-green-600 rounded-md p-1.5">
+                  <LuChevronsLeft color="green" size={20} />
+                </button>
+                <button className="border bg-green-100 border-green-600 rounded-md p-1.5">
+                  <LuChevronLeft color="green" size={20} />
+                </button>
+                <button className="border bg-green-100 border-green-600 rounded-md p-1.5">
+                  <LuChevronRight color="green" size={20} />
+                </button>
+                <button className="border bg-green-100 border-green-600 rounded-md p-1.5">
+                  <LuChevronsRight color="green" size={20} />
+                </button>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tfoot>
     </table>
   );
 };
