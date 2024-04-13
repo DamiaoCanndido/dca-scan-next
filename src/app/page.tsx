c5b10aee36e6ebf4dba002d923428f7f206fc908
DamiaoCanndido <tower_gamer@hotmail.com>
2024-04-12 21:33:27 -0300

    change login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,94 @@
 'use client';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { LuEye, LuEyeOff } from 'react-icons/lu';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Page() {
   const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [typePW, setTypePW] = useState('password');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+    if (!showPassword) {
+      setTypePW('text');
+    } else {
+      setTypePW('password');
+    }
+  };
+
   return (
-    <div className="flex fixed items-center justify-center w-screen h-screen">
-      <div className="flex items-center justify-center w-96 h-96 flex-col">
-        <Input type="text" placeholder="chave" />
-        <Label className="m-3">Insira sua chave.</Label>
-        <Button className="w-full" onClick={() => router.replace('/create')}>
-          Entrar
-        </Button>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Image
+          className="mx-auto"
+          src="/logo.png"
+          alt="DCA Scan"
+          width={80}
+          height={80}
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Entre na sua conta
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="E-mail"
+              autoComplete="email"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-dark sm:text-sm sm:leading-6"
+            />
+          </div>
+
+          <div className="relative mt-2">
+            <input
+              id="password"
+              name="password"
+              type={typePW}
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Senha"
+              autoComplete="current-password"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-dark sm:text-sm sm:leading-6"
+            />
+            <button
+              onClick={toggleShowPassword}
+              className="absolute top-1/2 right-3 -translate-y-1/2"
+            >
+              {showPassword ? <LuEye /> : <LuEyeOff />}
+            </button>
+          </div>
+
+          <div>
+            <button
+              onClick={() => router.replace('/create')}
+              type="button"
+              className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Entrar
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
