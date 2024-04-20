44fda5f39be9447f4975d43d527d3a321c41b2f1
DamiaoCanndido <tower_gamer@hotmail.com>
2024-04-19 21:48:23 -0300

    add validation on create-dialog

--- src/components/owner/create-dialog.tsx
+++ src/components/owner/create-dialog.tsx
@@ -3,14 +3,24 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
   DialogClose,
 } from '@/components/ui/dialog';
-import { useState } from 'react';
 import { TodoList } from '@/entities/api-data';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 interface HTMLProps {
   myDiv?: JSX.Element;
@@ -28,25 +38,34 @@ interface HTMLProps {
 }
 
 export const CreateDialog = (props: HTMLProps) => {
-  const [description, setDescription] = useState('');
-  const [order, setOrder] = useState('');
-  const [createdAt, setCreatedAt] = useState('');
+  const formSchema = z.object({
+    description: z
+      .string()
+      .min(3, { message: 'A descrição deve conter no mínimo 6 caracteres.' })
+      .max(50, { message: 'A descrição não pode exceder 50 caracteres.' }),
+    order:
+      props.data.slug === '/law'
+        ? z.string().min(1, { message: 'A ordem está faltando.' })
+        : z.optional(z.string()),
+    createdAt: z.optional(z.string()),
+  });
 
-  const handleDescriptionChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setDescription(event.target.value);
-  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: '',
+      order: '',
+      createdAt: '',
+    },
+  });
 
-  const handleOrderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setOrder(event.target.value);
-  };
-
-  const handleCreatedAtChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setCreatedAt(event.target.value);
-  };
+  async function onSubmit({
+    order,
+    description,
+    createdAt,
+  }: z.infer<typeof formSchema>) {
+    props.func({ order, description, createdAt });
+  }
 
   return (
     <Dialog>
@@ -56,57 +75,71 @@ export const CreateDialog = (props: HTMLProps) => {
           <DialogTitle>{props.title}</DialogTitle>
           <DialogDescription>{props.description}</DialogDescription>
         </DialogHeader>
-        {props.data.slug === '/law' && props.action !== 'Deletar' ? (
-          <div className="flex py-4">
-            <input
-              id="order"
-              value={order}
-              onChange={handleOrderChange}
-              placeholder="ordem"
-              className="flex w-max"
-            />
-          </div>
-        ) : (
-          <></>
-        )}
-        {props.action !== 'Deletar' ? (
-          <div className="flex pb-4">
-            <input
-              id="description"
-              placeholder="descrição"
-              value={description}
-              className="flex w-max"
-              onChange={handleDescriptionChange}
-            />
-          </div>
-        ) : (
-          <></>
-        )}
-        {props.data.slug === '/law' && props.action !== 'Deletar' ? (
-          <div className="flex pb-4">
-            <input
-              id="data"
-              value={createdAt}
-              type="datetime-local"
-              placeholder="data"
-              className="flex w-max"
-              onChange={handleCreatedAtChange}
-            />
-          </div>
-        ) : (
-          <></>
-        )}
-        <DialogFooter>
-          <DialogClose className="bg-green-600 m-2 w-20 h-8 rounded-sm">
-            <div
-              onClick={() =>
-                props.func({ description, order, createdAt, id: props.data.id })
-              }
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            {props.data.slug === '/law' && props.action !== 'Deletar' ? (
+              <FormField
+                control={form.control}
+                name="order"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>*Ordem (Obrigatório)</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Ordem" type="number" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ) : (
+              <></>
+            )}
+            {props.action !== 'Deletar' ? (
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>*Descrição (Obrigatório)</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Descrição" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ) : (
+              <></>
+            )}
+            {props.data.slug === '/law' && props.action !== 'Deletar' ? (
+              <FormField
+                control={form.control}
+                name="createdAt"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Data</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="datetime-local"
+                        placeholder="Data"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ) : (
+              <></>
+            )}
+            <DialogClose
+              type="submit"
+              className="bg-green-600 h-8 rounded-sm w-full"
             >
-              <p className="text-white">{props.action}</p>
-            </div>
-          </DialogClose>
-        </DialogFooter>
+              <p className="text-white">Criar</p>
+            </DialogClose>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
